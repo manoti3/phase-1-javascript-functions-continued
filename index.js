59d7021d9e7074d00e6e8102a1bf889ae1c2e651
manoti3 <hillarymanoti0@gmail.com>
2023-12-21 13:05:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    console.log("roller-skate")
+}
+
+function mondayWork(){
+    console.log("go to the office")
+}
+
+function wrapAdjective(){
+    console.log("");
+}
+
